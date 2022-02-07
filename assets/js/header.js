9ab6c74bd5dd56b308630f28955d9ab9b2c97b7a
Agustin Somoza Polchi <aspolchi@gmail.com>
2022-02-07 01:11:42 -0300

    starting a new

---
+++ assets/js/header.js
@@ -0,0 +1,7 @@
+export const HEADER = $('.header').append(`<div class="header__box">
+<ul class="header__box__ul">
+    <li class="header__box__ul--li">Agustin's Profile</li>
+    <li class="header__box__ul--li">Agustin's Skills</li>
+    <li class="header__box__ul--li">Agustin's Portfolio</li>
+</ul>
+</div>`);
---
+++ assets/js/index.js
@@ -0,0 +1,3 @@
+import { HEADER } from "./header.js";
+
+$("body").append(HEADER)
